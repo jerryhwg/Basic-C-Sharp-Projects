948ce51674649a5056005e584e32eb21974a926e
Jerry Hwang <jerryhwang@outlook.com>
2018-11-26 11:59:52 -0800

    update

--- anonyIncomeComp/anonyIncomeComp/Program.cs
+++ anonyIncomeComp/anonyIncomeComp/Program.cs
@@ -13,22 +13,35 @@ namespace anonyIncomeComp
             int weeksYr = 52;
             Console.WriteLine("Anonymous Income Comparison Program");
             Console.WriteLine("Person 1");
-            int person1HrRate = 10;
-            Console.WriteLine("Hourly Rate = " + person1HrRate);
-            int person1HrWeek = 40;
-            Console.WriteLine("Hours worked per week = " + person1HrWeek);
+            Console.WriteLine("Person 1 Hourly Rate?");
+            string person1HrRate = Console.ReadLine();
+            int person1HrRateNum = Convert.ToInt32(person1HrRate);
+            //Console.WriteLine("Person 1 Hourly Rate = " + person1HrRateNum);
+            Console.WriteLine("Person 1 Hours Worked Per Week?");
+            string person1HrWeek = Console.ReadLine();
+            int person1HrWeekNum = Convert.ToInt32(person1HrWeek);
+            //Console.WriteLine("Person 1 Hours worked per week = " + person1HrWeekNum);
+            int person1WkIncome = person1HrRateNum * person1HrWeekNum;
+            Console.WriteLine("Person 1 weekly income = " + person1WkIncome);
             Console.WriteLine("Person 2");
-            int person2HrRate = 8;
-            Console.WriteLine("Hourly Rate = " + person2HrRate);
-            int person2HrWeek = 40;
-            Console.WriteLine("Hours worked per week = " + person2HrWeek);
-            int persona1YrIncome = person1HrRate * person1HrWeek * weeksYr;
-            Console.WriteLine("Annual salary of Person 1 = " + persona1YrIncome);
-            int persona2YrIncome = person2HrRate * person2HrWeek * weeksYr;
-            Console.WriteLine("Annual salary of Person 2 = " + persona2YrIncome);
+            Console.WriteLine("Person 2 Hourly Rate?");
+            string person2HrRate = Console.ReadLine();
+            int person2HrRateNum = Convert.ToInt32(person2HrRate);
+            //Console.WriteLine("Person 2 Hourly Rate = " + person2HrRateNum);
+            Console.WriteLine("Person 2 Hours Worked Per Week?");
+            string person2HrWeek = Console.ReadLine();
+            int person2HrWeekNum = Convert.ToInt32(person2HrWeek);
+            //Console.WriteLine("Person 2 Hours worked per week = " + person2HrWeekNum);
+            int person2WkIncome = person2HrRateNum * person2HrWeekNum;
+            Console.WriteLine("Person 2 weekly income = " + person1WkIncome);
+            int person1YrIncome = person1HrRateNum * person1HrWeekNum * weeksYr;
+            Console.WriteLine("Annual salary of Person 1 = " + person1YrIncome);
+            int person2YrIncome = person2HrRateNum * person2HrWeekNum * weeksYr;
+            Console.WriteLine("Annual salary of Person 2 = " + person2YrIncome);
             Console.WriteLine("Does Person 1 make more momeny than Person 2?");
-            bool trueOrFalse = persona1YrIncome > persona2YrIncome;
+            bool trueOrFalse = person1YrIncome > person2YrIncome;
             Console.WriteLine(trueOrFalse);
+
             Console.Read();
         }
     }
