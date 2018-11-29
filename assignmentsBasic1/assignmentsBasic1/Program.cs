c39de9283db3470d654216eb87f46234fbf8f625
Jerry Hwang <jerryhwang@outlook.com>
2018-11-28 17:03:06 -0800

    update

--- assignmentsBasic1/assignmentsBasic1/Program.cs
+++ assignmentsBasic1/assignmentsBasic1/Program.cs
@@ -6,7 +6,8 @@ class Program
 {
     static void Main()
     {
-        // Assignment 1
+        // Assignment 1: Create a one-dimensional Array of strings. Ask the user to input some text. Create a loop that goes through each string in the Array, adding the user’s text to the string. 
+        // Then create a loop that prints off each string in the Array on a separate line.
         string[] names = { "Jesse", "Jerry", "Tom" };
         Console.WriteLine("Say \"Hi\" to eaveryone");
         string greet = Console.ReadLine();
@@ -16,7 +17,8 @@ class Program
             Console.WriteLine(greet + " " + names[i] + "!");
         }
 
-        // Assignment 2 & 3
+        // ASsignment 2: Create an infinite loop.
+        // Assignment 3: Fix the infinite loop so it will execute.
         Console.WriteLine("\nGuess my number?");
         int number = Convert.ToInt32(Console.ReadLine());
         bool myNumber = number == 7;
@@ -39,7 +41,7 @@ class Program
 
         while (!myNumber);
 
-        // Assignment 4
+        // Assignment 4: Create a loop where the comparison used to determine whether to continue iterating the loop is a “<” operator.
 
         Console.WriteLine("Your test result: ");
         int[] myScores = { 70, 50, 45, 80, 90 };
@@ -53,7 +55,7 @@ class Program
             }
         }
 
-        // Assignment 5
+        // Assignment 5: Create a loop where the comparison used to determine whether to continue iterating the loop is a “<=” operator.
 
         Console.WriteLine("\nHow many treasures have you found?");
         int myTreasures = Convert.ToInt32(Console.ReadLine());
@@ -66,7 +68,10 @@ class Program
 
         Console.WriteLine("Good jobs, you have found enough treasures!");
 
-        // Assignment 6, 7 & 8
+        // Assignment 6: Create a List of strings where each item in the list is unique. Ask the user to select text to search for in the List. 
+        // Create a loop that iterates through the list and then displays the index of the array that contains matching text on the screen.
+        // Assignment 7: Add code to that above loop that tells a user if they put in text that isn’t in the List.
+        // Assignment 8: Add code to that above loop that stops it from executing once a match has been found.
 
         string[] fruits = { "apple", "banana", "orange", "pear", "grape" };
         Console.WriteLine("\nChoose what fruit you want in my fruit container?");
@@ -88,9 +93,48 @@ class Program
             }
         }
 
-        // Assignment 9
+        // Assignment 9: Create a List of strings that has at least two identical strings in the List. Ask the user to select text to search for in the List. 
+        // Create a loop that iterates through the list and then displays the indices of the array that contain matching text on the screen.
+        // Assignment 10: Add code to that above loop that tells a user if they put in text that isn’t in the List.
 
+        string[] city = { "seattle", "portland", "chicago", "newyork", "seattle" };
+        Console.WriteLine("\nChoose a city you would like to visit?");
+        string cityChoice = Console.ReadLine();
 
+        while (! city.Contains(cityChoice))
+        {
+            Console.WriteLine("Your choice is not valid");
+            Console.WriteLine("\nChoose a city you would like to visit?");
+            cityChoice = Console.ReadLine();
+        }
+
+        for (int l = 0; l < city.Length; l++)
+        {
+            if (city[l] == cityChoice)
+            {
+                Console.WriteLine("Your choice of city " + city[l] + " in our list.");
+            }
+        }
+
+        // Assignment 11: Create a List of strings that has at least two identical strings in the List. Create a foreach loop that evaluates each item in the list, and
+        // displays a message showing the string and whether or not it has already appeared in the list.
+
+        List<string> Names = new List<string>() { "Jesse", "Jerry", "Tom", "Jesse" };
+        Console.WriteLine("\nHere everyone's name.");
+
+        foreach (string name in Names)
+        {
+            Console.WriteLine(name);
+        }
+
+        foreach (string name in Names)
+        {
+            if (name.Count() > 1)
+            {
+                Console.WriteLine("\nLook the name " + name + " is in the list repeatedly");
+                break;
+            }
+        }
 
         Console.ReadLine();
     }
