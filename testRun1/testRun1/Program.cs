237880dcb45e73d5afe83bdfa4735fa73244f046
Jerry Hwang <jerryhwang@outlook.com>
2018-11-29 12:38:24 -0800

    update

--- testRun1/testRun1/Program.cs
+++ testRun1/testRun1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
     class Program
     {
@@ -12,17 +8,12 @@ using System.Threading.Tasks;
         Console.WriteLine("\nFInd a city name that is twice listed in the list.");
         string cityQuery = Console.ReadLine();
 
-        //int index = Array.IndexOf(city, cityQuery);
-        //Console.WriteLine("{0} is listed twice in the list at index {1}.", cityQuery, index);
-        //Console.ReadLine();
-
-        for (int i = 0; i < city.Length; i++)
+        int index = -1;
+        do
         {
-            if (cityQuery == city[i])
-            {
-                Console.WriteLine(i);
-            } 
-        }
+            index = Array.IndexOf(city, cityQuery, index + 1);
+            System.Console.WriteLine(index);
+        } while (-1 != index);
 
         Console.ReadLine();
 
