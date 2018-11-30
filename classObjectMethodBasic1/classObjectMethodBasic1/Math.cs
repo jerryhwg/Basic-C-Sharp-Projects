afc9bd5adc13d5a588b9af70c5514bcfd57e64ef
Jerry Hwang <jerryhwang@outlook.com>
2018-11-30 10:38:23 -0800

    update

---
+++ classObjectMethodBasic1/classObjectMethodBasic1/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classObjectMethodBasic1
+{
+    public class Math
+    {
+        public static double Addition(double number)
+        {
+            double result1 = (number + 2);
+            return result1;
+        }
+
+        public static double Multiplication(double number)
+        {
+            double result2 = (number * 2);
+            return result2;
+        }
+
+        public static double Division(double number)
+        {
+            double result3 = (number / 2);
+            return result3;
+        }
+    }
+}
--- classObjectMethodBasic1/classObjectMethodBasic1/Program.cs
+++ classObjectMethodBasic1/classObjectMethodBasic1/Program.cs
@@ -11,38 +11,19 @@ namespace classObjectMethodBasic1
         public static void Main()
         {
             Console.WriteLine("What number do you want to do the math operations on?");
-            int number = Convert.ToInt32(Console.ReadLine());
+            double number = Convert.ToInt32(Console.ReadLine());
 
-            int result1 = new int();
-            result1 = Sum(result1);
-            int result2 = new int();
-            result2 = Product(result2);
-            int result3 = new int();
-            result3 = Quotient(result3);
+            double result1 = new double();
+            result1 = Math.Addition(number);
+            double result2 = new double();
+            result2 = Math.Multiplication(number);
+            double result3 = new double();
+            result3 = Math.Division(number);
 
-            Console.WriteLine("Math 1 result: " + result1);
-            Console.WriteLine("Math 2 result: " + result2);
-            Console.WriteLine("Math 3 result: " + result3);
+            Console.WriteLine("Addition result: " + result1);
+            Console.WriteLine("Multiplication result: " + result2);
+            Console.WriteLine("Division result: " + result3);
             Console.ReadLine();
-        }
-
-        public static int Sum(int number)
-
-        {
-            int result1 = number + 2;
-            return result1;
-        }
-
-        public static int Product(int number)
-        {
-            int result2 = number * 2;
-            return result2;
-        }
-
-        public static int Quotient(int number)
-        {
-            int result3 = number / 2;
-            return result3;
         }
     }
 }
