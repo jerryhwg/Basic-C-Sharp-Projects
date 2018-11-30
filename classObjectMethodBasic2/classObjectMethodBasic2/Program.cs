32d026d2688b6e634e153d48a593e690b5b44f1a
Jerry Hwang <jerryhwang@outlook.com>
2018-11-30 12:58:06 -0800

    add

---
+++ classObjectMethodBasic2/classObjectMethodBasic2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classObjectMethodBasic2
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Math.math(firstNumber: 3, secondNumber: 5);
+        }
+    }
+
+    public class Math
+    {
+        public static void math(int firstNumber, int secondNumber)
+        {
+            int result = (firstNumber + 2);
+            Console.WriteLine(result);
+            Console.WriteLine(secondNumber);
+            Console.ReadLine();
+        }
+           }
+}
