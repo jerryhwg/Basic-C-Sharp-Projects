51c841249487a6d194e974d61dda3bb4cab7ff46
Jerry Hwang <jerryhwang@outlook.com>
2018-12-04 11:48:45 -0800

    update

--- InterfaceBasic1/InterfaceBasic1/Employee.cs
+++ InterfaceBasic1/InterfaceBasic1/Employee.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AbstractClassBasic1
+namespace InterfaceBasic1
 {
-    public class Employee : Person // inheritance
+    public class Employee : Person, IQuittable // Person: class inheritance, IQuittable: interface inheritiance
     {
         public new void SayName()
         {
             Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Quit Quit");
         }
     }
 }
--- InterfaceBasic1/InterfaceBasic1/Person.cs
+++ InterfaceBasic1/InterfaceBasic1/Person.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AbstractClassBasic1
+namespace InterfaceBasic1
 {
     public abstract class Person
     {
