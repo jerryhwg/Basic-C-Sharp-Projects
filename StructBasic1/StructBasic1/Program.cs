ae7789c388abbb720a7aa41608658aced0dcc95e
Jerry Hwang <jerryhwang@outlook.com>
2018-12-05 19:19:53 -0800

    Add

---
+++ StructBasic1/StructBasic1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructBasic1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Number data = new Number(); // instantiate, create an object 'data' with data type 'Number'
+            data.Amount = 100; // assign an amount to 'data' object(instance)
+            Console.WriteLine(data.Amount); // print the amount to the console
+            Console.ReadLine();
+        }
+    public struct Number // create a struct called Number
+    {
+        public decimal Amount { get; set; } // propery 'Amount' with decimal datatype
+    }
+        
+    }
+}
