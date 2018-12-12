e7faf03440ceff1933e44363608c5e37948f8acf
Jerry Hwang <jerryhwang@outlook.com>
2018-12-11 22:35:59 -0800

    update

---
+++ TechAcadStudentsMVC/TechAcadStudentsMVC/Models/Instructor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechAcadStudentsMVC.Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    } // a model to store and display data
+}
